0d8e383a22c51ac135f049f3648ae90833498097
chsakell <chsakell@gmail.com>
2016-10-05 15:50:15 +0300

    remove unnecessary code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,25 +1,10 @@
-using System;
-using LiveGameFeed.Data.Abstract;
-using LiveGameFeed.Hubs;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR.Infrastructure;
 
 namespace LiveGameFeed.Controllers
 {
-    public class HomeController : ApiHubController<Broadcaster>
+    public class HomeController : Controller
     {
-        IMatchRepository _matchRepository;
-        IFeedRepository _feedRepository;
-
-        private Object lockOb = new Object();
-        public HomeController(IConnectionManager signalRConnectionManager,
-                              IMatchRepository matchRepository,
-                              IFeedRepository feedRepository)
-        : base(signalRConnectionManager)
-        {
-            _matchRepository = matchRepository;
-            _feedRepository = feedRepository;
-        }
+        public HomeController() { }
 
         public IActionResult Index()
         {
