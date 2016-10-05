8a8055d0ba53e5598a15ff01c99f1b8164af5b75
chsakell <chsakell@gmail.com>
2016-10-05 10:58:14 +0300

    fix highlight directive

--- app/shared/directives/highlight.directive.ts
+++ app/shared/directives/highlight.directive.ts
@@ -5,16 +5,13 @@ import { Directive, ElementRef, HostListener, Input, Renderer } from '@angular/c
 })
 export class HighlightDirective {
   constructor(private el: ElementRef, private renderer: Renderer) {
-      this.renderer.setElementClass(this.el.nativeElement, 'feed-highlight', true);
+    let self = this;
+      self.renderer.setElementClass(this.el.nativeElement, 'feed-highlight', true);
+      setTimeout(function() {
+        console.log('removing..');
+        self.renderer.setElementClass(self.el.nativeElement,'feed-highlight-light', true);
+      }, 1000);
    }
-
-  @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('white');
-  }
-
-  @HostListener('mouseleave') onMouseLeave() {
-    this.highlight(null);
-  }
 
   private highlight(color: string) {
     this.renderer.setElementStyle(this.el.nativeElement, 'backgroundColor', color);
