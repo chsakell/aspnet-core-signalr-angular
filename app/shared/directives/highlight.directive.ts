694cb912e8af816111e1b8abf69b341999697f7d
chsakell <chsakell@gmail.com>
2016-12-06 16:36:04 +0200

    add feed indication

--- app/shared/directives/highlight.directive.ts
+++ app/shared/directives/highlight.directive.ts
@@ -6,11 +6,15 @@ import { Directive, ElementRef, HostListener, Input, Renderer } from '@angular/c
 export class HighlightDirective {
   constructor(private el: ElementRef, private renderer: Renderer) {
     let self = this;
-      self.renderer.setElementClass(this.el.nativeElement, 'feed-highlight', true);
-      setTimeout(function() {
-        self.renderer.setElementClass(self.el.nativeElement,'feed-highlight-light', true);
-      }, 1000);
-   }
+    self.renderer.setElementClass(this.el.nativeElement, 'feed-highlight', true);
+    let horn = document.getElementById('horn');
+    horn.className += ' orange';
+    setTimeout(function () {
+      self.renderer.setElementClass(self.el.nativeElement, 'feed-highlight-light', true);
+
+      document.getElementById("horn").className = document.getElementById("horn").className.replace(/(?:^|\s)orange(?!\S)/g, '')
+    }, 1000);
+  }
 
   private highlight(color: string) {
     this.renderer.setElementStyle(this.el.nativeElement, 'backgroundColor', color);
